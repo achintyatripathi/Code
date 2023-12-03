69ce15fd7f51778fd8a7f5bea7398b27f2ffb9c3
achintya.tripathi <achintya_tripathi@apple.com>
2023-12-03 18:28:19 +0530

    updated dutch flag with initial code

---
+++ Dutch Flag.py
@@ -0,0 +1,31 @@
+# version 1.0
+# this is a version of dutch flag problem 
+# it will have a list on 0,1,2 no. throughout the list , we need to sort them and 
+# arrange the same in a manner where all 0 ,1, 2 are linked one after other.
+
+
+def dutch(a : list) -> list :
+    
+    left , mid = 0,0
+    right = len(a) -1
+
+    # we can follow the binary search method here . 
+    # find mid here. 
+    # 
+    # mid == 0  then  l,mid = mid,l   l+=1 , mim+=1 
+    # mid == 1 then l=mid +=1 
+    # mid == 2  then ,mid = mid,r 
+
+    while mid <= right:
+
+        if a[mid] == 0 :
+            a[left],a[mid] = a[mid], a[left]
+            left +=1
+            mid+=1
+        elif a[mid] == 1:
+            left+=1 
+        else:
+            a[right],a[mid] = a[mid], a[right]
+            right -=1
+            mid+=1
+
