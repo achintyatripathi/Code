64291809d5372f7e56f8dbe9a7a7610b38cb0243
achintya.tripathi <achintya_tripathi@apple.com>
2023-12-06 08:20:31 +0530

    longest subarray commited

--- Longest Sub_array sum.py
+++ Longest Sub_array sum.py
@@ -1,15 +1,24 @@
 
-
+l = list(map(int,input("Enter list of numbers \n").split()))
+k = int(input("Enter  numbers \n"))
 def sub_array(a:list,k: int) -> list:
 
-    max_sum = 0
+    maxL = 0
     cur_sum = 0
-
+    d = {}
     for i in range(len(a)):
 
         cur_sum+=a[i]
 
         if cur_sum == k:
-            return ma   
-        max_sum = max(max_sum,cur_sum)
+            maxL = i+1  
+        elif (cur_sum - k) in d :
+            maxL = max(maxL,i - d[cur_sum-k])
+        
+        if cur_sum not in d:
+            d[cur_sum] = i 
+    
+    return maxL
+
+print(sub_array(l,k))
 
